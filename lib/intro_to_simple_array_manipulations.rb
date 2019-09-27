20b51a294a84ba556d8a88bd5dad36af1f499f54
Nicole M Darby <nicoledarby021322@gmail.com>
2019-09-27 15:37:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,10 @@
-artists=["Hozier","Ariana Grande","Usher"]
-next_artist="Beyonce"
-artist.push(next_artist)
+ artists=["Hozier","Ariana Grande","Usher"]
+ next_artist="Beyonce"
+ artist.push(next_artist)
 end
+
+ artist=["Frida Kahlo","Pable Picasso","Cai Guo-Qiang"]
+ artist.pop
+ artist#=> 
+
+ 
